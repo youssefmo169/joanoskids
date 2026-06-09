2dd3bc70f4396869a1b9a6a777a68829310310b6
youssefmo169 <ym999797@gmail.com>
2026-06-09 22:56:22 +0300

    Fix translation and add 'free' text in LanguageContext

--- JOE-main (3)/JOE-main/src/contexts/LanguageContext.tsx
+++ JOE-main (3)/JOE-main/src/contexts/LanguageContext.tsx
@@ -58,7 +58,7 @@ const translations = {
     startShopping: 'ابدأ التسوق',
     subtotal: 'المجموع الفرعي',
     shipping: 'الشحن',
-    Dependonyourgovernment: 'يعتمد علي محافظتك',
+    free: 'مجاني',
     total: 'الإجمالي',
     paymentCod: 'الدفع: عند الاستلام',
     orderSummary: 'ملخص الطلب',
