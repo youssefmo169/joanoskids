1e5e4a792750bd9a89c56fd153b28f8fed157468
youssefmo169 <ym999797@gmail.com>
2026-06-12 15:40:03 +0300

    Add Facebook Pixel tracking component

--- JOE-main (3)/JOE-main/src/App.tsx
+++ JOE-main (3)/JOE-main/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import { CartProvider } from './contexts/CartContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { LanguageProvider } from './contexts/LanguageContext';
@@ -16,6 +17,38 @@ import AdminLoginPage from './pages/admin/AdminLoginPage';
 import AdminDashboardPage from './pages/admin/AdminDashboardPage';
 import ScrollToTop from './components/ScrollToTop';
 import ContactPage from './pages/ContactPage';
+import { supabase } from './lib/supabase';
+
+function FacebookPixel() {
+  useEffect(() => {
+    supabase
+      .from('settings')
+      .select('value')
+      .eq('key', 'facebook_pixel_id')
+      .single()
+      .then(({ data }) => {
+        const pixelId = data?.value?.trim();
+        if (!pixelId) return;
+
+        const script = document.createElement('script');
+        script.innerHTML = `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${pixelId}');
+          fbq('track', 'PageView');
+        `;
+        document.head.appendChild(script);
+      });
+  }, []);
+
+  return null;
+}
 
 export default function App() {
   return (
@@ -23,6 +56,7 @@ export default function App() {
       <LanguageProvider>
         <AuthProvider>
           <CartProvider>
+            <FacebookPixel />
             <ScrollToTop />
             <Routes>
               <Route path="/admin/login" element={<AdminLoginPage />} />
